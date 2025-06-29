6c4f19e36b40bd1cecb7e17d21dfa363507505e3
Aditya Shukla <Shadowshukla76@gmail.com>
2025-06-29 15:07:10 +0530

    Added More Fields in pydantic Company Schema

--- src/schema/CompanySchema.py
+++ src/schema/CompanySchema.py
@@ -1,25 +1,38 @@
 from pydantic import BaseModel, EmailStr
 from typing import List, Optional
+from datetime import datetime
+
 
 class CompanyBase(BaseModel):
     company_name: str
     company_email: EmailStr
+    createdAt: datetime
+    updatedAt: datetime
+    address: str
+    companyLogo: str
+    phoneNumber: str
+    timezone: str
+
 
 class CompanyCreate(CompanyBase):
     password: str
 
+
 class CompanyLogin(BaseModel):
     company_email: EmailStr
     password: str
+
 
 class CompanyRead(CompanyBase):
     id: int
     company_code: str
     model_config = {"from_attributes": True}
 
+
 class CompanyFull(CompanyRead):
     employees: Optional[List["EmployeeRead"]] = []  # forward reference
     model_config = {"from_attributes": True}
+
 
 class TokenSent(BaseModel):
     message: str
@@ -28,6 +41,7 @@ class TokenSent(BaseModel):
     token: str
     token_type: str
 
+
 class CompanyResponse(BaseModel):
     success: bool
     message: Optional[str] = None
@@ -35,6 +49,7 @@ class CompanyResponse(BaseModel):
 
 
 from typing import TYPE_CHECKING
+
 if TYPE_CHECKING:
     from .CompanySchema import CompanyRead
     from .EmployeeSchema import EmployeeRead
